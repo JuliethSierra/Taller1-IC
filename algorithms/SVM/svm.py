2f05408197450db6c6a6a03275bb6765b07af8f5
CesarAcero97 <56569713+CesarAcero97@users.noreply.github.com>
2023-09-06 10:29:10 -0500

    Update svm.py

--- algorithms/SVM/svm.py
+++ algorithms/SVM/svm.py
@@ -3,11 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.calibration import CalibratedClassifierCV
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-from sklearn.calibration import CalibratedClassifierCV
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
