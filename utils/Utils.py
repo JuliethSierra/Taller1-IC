90ba58e47cba4caba662e4fe769dc1030e7ad7e3
Andrea Sierra <andrea.sierra05@uptc.edu.co>
2023-09-05 21:26:44 -0500

    Update ModifyData.py

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,39 +1,3 @@
-"""import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-
-# Cargar el archivo CSV en un DataFrame
-data = pd.read_csv("Prueba1.csv")
-
-# Dividir los datos en características (X) y variable objetivo (y)
-print(data.columns)
-X = data.drop("Aprobado", axis=1)
-y = data["Aprobado"]
-
-# Dividir los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-# Normalizar los datos
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-# Crear y entrenar el modelo de regresión logística
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-# Realizar predicciones en el conjunto de prueba
-y_pred = model.predict(X_test)
-
-# Calcular la precisión del modelo
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Precisión del modelo: {accuracy:.2f}')
-"""
-
-
-#https://www.aprendemachinelearning.com/regresion-logistica-con-python-paso-a-paso/#more-5394
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
@@ -43,11 +7,10 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import seaborn as sb
-#%matplotlib inline
 
-dataframe = pd.read_csv(r"data_finales.csv")
-dataframe.head()
-dataframe.describe()
+dataframe = pd.read_csv(r"data\data_finales.csv")
+#dataframe.head()
+#dataframe.describe()
 #print(dataframe.groupby('Aprobado').size())
 
 #dataframe.drop(['Aprobado'], axis=1)
@@ -55,61 +18,13 @@ dataframe.describe()
 #plt.show()
 
 #sb.pairplot(dataframe.dropna(), hue='Aprobado',height=4,vars=["Matematicas","Lenguaje","Ciencias","Ingles"],kind='reg')
-print(dataframe)
+#print(dataframe)
 #plt.show()
 
-sb.heatmap(dataframe.isnull(), cmap='rainbow') #Mostrar campos nulos con un mapa de calor
-plt.show()
+#sb.heatmap(dataframe.isnull(), cmap='rainbow') #Mostrar campos nulos con un mapa de calor
+#plt.show()
 
-#crear el modelo
-"""X = np.array(dataframe.drop(['Aprobado'], axis=1))
-y = np.array(dataframe['Aprobado'])
-X.shape
-
-model = linear_model.LogisticRegression()
-model.fit(X,y)
-
-predictions = model.predict(X)
-#predictions[0:10] imprime los primeros 10 datos de la predicción
-print(predictions[0:10])
-accuracy=model.score(X,y)
-print(f'Precisión del modelo: {accuracy:.2f}')
-
-#Validación del modelo
-
-#dividir los datos
-validation_size = 0.30
-seed = 7
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
-
-
-name='Logistic Regression'
-kfold = model_selection.KFold(n_splits=10, random_state=None)
-cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-print(msg)
-
-predictions1 = model.predict(X_validation)
-print(accuracy_score(Y_validation, predictions1))
-
-print("\nMatriz de confusión")
-print(confusion_matrix(Y_validation, predictions1))
-
-
-print("\nReporte de clasificación")
-print(classification_report(Y_validation, predictions1))
-
-
-dataframe = pd.read_csv(r"Saber_11__2019-2.csv")
-dataframe.head()
-dataframe.describe()
-print(dataframe.groupby('ESTU_GENERACION-E').size())
-dataframe.drop(['ESTU_GENERACION-E'], axis=1)
-dataframe.hist()
-plt.show()
-#sb.pairplot(dataframe.dropna(), hue='ESTU_GENERACION-E',size=10,vars=["DESEMP_SOCIALES_CIUDADANAS","PUNT_INGLES","PERCENTIL_INGLES","DESEMP_INGLES","PUNT_GLOBAL","PERCENTIL_GLOBAL","ESTU_INSE_INDIVIDUAL", "ESTU_NSE_INDIVIDUAL","ESTU_NSE_ESTABLECIMIENTO","ESTU_ESTADOINVESTIGACION"],kind='reg')
-#sb.pairplot(dataframe.dropna(), hue='ESTU_GENERACION-E',height=4,vars=["PERCENTIL_GLOBAL","ESTU_INSE_INDIVIDUAL", "ESTU_NSE_INDIVIDUAL","ESTU_NSE_ESTABLECIMIENTO"],kind='reg')
+dataframe.drop(['ESTU_GENERACION-E'], axis=1).hist() #visualización de datos
 plt.show()
 
 
-"""
--- utils/clearData/ModifyData.py
+++ utils/clearData/ModifyData.py
@@ -2,25 +2,92 @@ import pandas as pd
 import re
 
 # Lee el archivo CSV en un DataFrame
-archivo_csv = "Saber_11__2019-2.csv"
+archivo_csv = "Taller/example/filas_sin_nulos.csv"
 df = pd.read_csv(archivo_csv, low_memory=False)
 
 # Modifica la columna "Aprobado" utilizando la función replace
 df['FAMI_ESTRATOVIVIENDA'] = df['FAMI_ESTRATOVIVIENDA'].replace({'Sin Estrato': 0, 'Estrato 1':1, 'Estrato 2': 2, 'Estrato 3': 3, 'Estrato 4': 4, 'Estrato 5': 5, 'Estrato 6': 6})
 df['ESTU_GENERO'] = df['ESTU_GENERO'].replace({'M': 1, 'F': 2})
-df['FAMI_TIENEINTERNET'] = df['FAMI_TIENEINTERNET'].replace({'Si': 1, 'No': 2})
-df['FAMI_TIENECOMPUTADOR'] = df['FAMI_TIENECOMPUTADOR'].replace({'Si': 1, 'No': 2})
-df['COLE_BILINGUE'] = df['COLE_BILINGUE'].replace({'S': 1, 'N': 2})
+df['FAMI_TIENEINTERNET'] = df['FAMI_TIENEINTERNET'].replace({'Si': 1, 'No': 0})
+df['FAMI_TIENECOMPUTADOR'] = df['FAMI_TIENECOMPUTADOR'].replace({'Si': 1, 'No': 0})
+df['COLE_BILINGUE'] = df['COLE_BILINGUE'].replace({'S': 1, 'N': 0})
+df['ESTU_GENERACION-E'] = df['ESTU_GENERACION-E'].replace({'GENERACION E - EXCELENCIA NACIONAL': 1, 'GENERACION E - EXCELENCIA DEPARTAMENTAL':1, 'GENERACION E - GRATUIDAD': 1, 'NO': 0})
+df['FAMI_PERSONASHOGAR'] = df['FAMI_PERSONASHOGAR'].replace({'1 a 2': 4, '3 a 4':3, '5 a 6': 2, '7 a 8': 1,'9 o más': 0})
+df['FAMI_TIENESERVICIOTV'] = df['FAMI_TIENESERVICIOTV'].replace({'Si': 1, 'No': 0})
 
-df['ESTU_GENERACION-E'] = df['ESTU_GENERACION-E'].replace({'GENERACION E - EXCELENCIA NACIONAL': 1, 'GENERACION E - EXCELENCIA DEPARTAMENTAL':1, 'GENERACION E - GRATUIDAD': 1, 'NO': 2})
+
+df['FAMI_EDUCACIONPADRE'] = df['FAMI_EDUCACIONPADRE'].replace({
+    'Educación profesional completa': 8,
+    'Secundaria (Bachillerato) completa': 4,
+    'Técnica o tecnológica completa': 7,
+    'No sabe': 0,
+    'Secundaria (Bachillerato) incompleta': 3,
+    'Primaria completa': 2,
+    'Educación profesional incompleta': 6,
+    'Postgrado': 9,
+    'Técnica o tecnológica incompleta': 5,
+    'No Aplica': 0,
+    'Primaria incompleta': 1,
+    'Ninguno': 0
+})
+
+# Formatea la variable FAMI_EDUCACIONMADRE
+df['FAMI_EDUCACIONMADRE'] = df['FAMI_EDUCACIONMADRE'].replace({
+    'Educación profesional completa': 8,
+    'Secundaria (Bachillerato) completa': 4,
+    'Técnica o tecnológica completa': 7,
+    'No sabe': 0,
+    'Secundaria (Bachillerato) incompleta': 3,
+    'Primaria completa': 2,
+    'Educación profesional incompleta': 6,
+    'Postgrado': 9,
+    'Técnica o tecnológica incompleta': 5,
+    'No Aplica': 0,
+    'Primaria incompleta': 1,
+    'Ninguno': 0
+})
+
+# Formatea la variable FAMI_NUMLIBROS
+df['FAMI_NUMLIBROS'] = df['FAMI_NUMLIBROS'].replace({
+    'MÁS DE 100 LIBROS': 3,
+    '26 A 100 LIBROS': 2,
+    '0 A 10 LIBROS': 0,
+    '11 A 25 LIBROS': 1
+})
+
+# Formatea la variable ESTU_DEDICACIONLECTURADIARIA
+df['ESTU_DEDICACIONLECTURADIARIA'] = df['ESTU_DEDICACIONLECTURADIARIA'].replace({
+    'Entre 30 y 60 minutos': 2,
+    'No leo por entretenimiento': 0,
+    '30 minutos o menos': 1,
+    'Entre 1 y 2 horas': 3,
+    'Más de 2 horas': 4
+})
+
+# Formatea la variable ESTU_DEDICACIONINTERNET
+df['ESTU_DEDICACIONINTERNET'] = df['ESTU_DEDICACIONINTERNET'].replace({
+    'Entre 30 y 60 minutos': 2,
+    'Más de 3 horas': 0,
+    'Entre 1 y 3 horas': 1,
+    'No Navega Internet': 4,
+    '30 minutos o menos': 3
+})
+
+# Formatea la variable ESTU_HORASSEMANATRABAJA
+df['ESTU_HORASSEMANATRABAJA'] = df['ESTU_HORASSEMANATRABAJA'].replace({
+    'Menos de 10 horas': 3,
+    '0': 4,  # Aquí asumo que '0' significa cero horas trabajadas
+    'Más de 30 horas': 0,
+    'Entre 11 y 20 horas': 2,
+    'Entre 21 y 30 horas': 1
+})
+
 
 # Genera un nuevo archivo CSV con las modificaciones
-nuevo_archivo_csv = "data_finales.csv"
+nuevo_archivo_csv = "Taller/example/data_formateada_k.csv"
 df.to_csv(nuevo_archivo_csv, index=False)
 
 print(f"Valores con caracteres especiales o vacíos modificados y columna 'FAMI_TIENEINTERNET' modificada, y guardados en el archivo '{nuevo_archivo_csv}'.")
-
-
 
 #One hot encoding
 #MinMaxS
